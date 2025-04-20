b8ce3ae2295edacb81e78909935c45a81971cd9d
Heliomar P. Marques <heliomarpm@gmail.com>
2025-04-20 00:33:31 -0300

    fix: update ESLint and Vitest configuration files for improved path resolution and formatting

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		globals: true,
+		reporters: ['verbose']
 		// environment: 'jsdom', // Use jsdom for browser-like tests
 		// coverage: {
 		// 	reporter: ['text', 'json', 'html'] // Optional: Add coverage reports
